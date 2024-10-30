423b8f30a322124f6ba4d7fbd2c2e980ec9515a1
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2024-10-30 13:25:02 +0000

    revisao

---
+++ Aulas/aula_05/revisao.py
@@ -0,0 +1,7 @@
+import math
+class Retangulo():
+# função construtora (init) define os atributos da classe
+# classe engloba o geral, enquanto as outras criam subdivisões
+    def __init__():
+        self.__altura = 0
+        self.__base = 0
