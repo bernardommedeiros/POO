d837fa30f8a1fb653bba82be4d39e40c4f279636
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2024-12-05 11:23:15 +0000

    streamlit

---
+++ Equação2Grau/equacao.py
@@ -0,0 +1,12 @@
+class Equacao:
+    def __init__(self, a, b, c):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+    def __str__(self):
+        return f"Base = {self.__b} Altura = {self.__h}"
+    def calc_area(self):
+        
+    def calc_diagonal(self):
+        return math.sqrt(a*x**2 + b*x + c)
+
---
+++ Equação2Grau/index.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import equacaoUI import EquacaoUI
+st.header("Cálculo: Equação do II Grau")
+
+
+
